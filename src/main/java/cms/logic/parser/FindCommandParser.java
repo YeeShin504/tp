e8dc55d529d2728422693e75c1d9518205801845
derpymoo <zhengyijie183@gmail.com>
2026-03-17 22:39:05 +0800

    Add find by NUS_ID

--- src/main/java/cms/logic/parser/FindCommandParser.java
+++ src/main/java/cms/logic/parser/FindCommandParser.java
@@ -1,12 +1,17 @@
 package cms.logic.parser;
 
 import static cms.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static cms.logic.parser.CliSyntax.PREFIX_NAME;
+import static cms.logic.parser.CliSyntax.PREFIX_NUSID;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import cms.logic.commands.FindCommand;
 import cms.logic.parser.exceptions.ParseException;
 import cms.model.person.NameContainsKeywordsPredicate;
+import cms.model.person.NusIdContainsKeywordsPredicate;
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -26,8 +31,38 @@ public class FindCommandParser implements Parser<FindCommand> {
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
+        // Tokenize for optional prefixes
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_NUSID);
 
+        boolean hasName = argMultimap.getValue(PREFIX_NAME).isPresent();
+        boolean hasNusId = argMultimap.getValue(PREFIX_NUSID).isPresent();
+
+        if (hasName && hasNusId) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+
+        if (hasName) {
+            String nameArg = argMultimap.getValue(PREFIX_NAME).get().trim();
+            if (nameArg.isEmpty()) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+            }
+            List<String> nameKeywords = Arrays.stream(nameArg.split("\\s+"))
+                    .collect(Collectors.toList());
+            return new FindCommand(new NameContainsKeywordsPredicate(nameKeywords));
+        }
+
+        if (hasNusId) {
+            String idArg = argMultimap.getValue(PREFIX_NUSID).get().trim();
+            if (idArg.isEmpty()) {
+                throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+            }
+            List<String> idKeywords = Arrays.stream(idArg.split("\\s+"))
+                    .collect(Collectors.toList());
+            return new FindCommand(new NusIdContainsKeywordsPredicate(idKeywords));
+        }
+
+        // Legacy behavior: no prefixes -> treat entire trimmedArgs as name keywords
+        String[] nameKeywords = trimmedArgs.split("\\s+");
         return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
     }
 
